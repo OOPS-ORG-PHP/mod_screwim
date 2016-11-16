b712ccc0f9b495f64ff3d68c273c41edaf2a4385
JoungKyun Kim <joungkyun@gmail.com>
2016-11-16 12:44:06 +0900

    add cleaner

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+[ -f Makefile ] && make distclean
+
+rm -rf include autom4te* build modules
+rm -f Makefile.g* Makefile.f* Makefile.o* Makefile
+rm -f config.log config.nice config.g* config.s* config.h.* ac* configure* mkinstalldirs
+rm -f ltmain.sh missing run-tests.php install* libtool tools/screw
