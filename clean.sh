8be4e1fc8c4d37d8e84796e7f24670b8234521df
JoungKyun Kim <joungkyun@gmail.com>
2016-11-17 00:52:36 +0900

    add config.h and .deps to be removed

--- clean.sh
+++ clean.sh
@@ -4,5 +4,5 @@
 
 rm -rf include autom4te* build modules
 rm -f Makefile.g* Makefile.f* Makefile.o* Makefile
-rm -f config.log config.nice config.g* config.s* config.h.* ac* configure* mkinstalldirs
-rm -f ltmain.sh missing run-tests.php install* libtool tools/screw
+rm -f config.log config.nice config.g* config.s* config.h* ac* configure* mkinstalldirs
+rm -f ltmain.sh missing run-tests.php install* libtool tools/screw .deps
